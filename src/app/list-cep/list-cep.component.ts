943333188aad41b7fd1af0af27ddaa424b8bc800
Rogério Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2021-05-01 20:31:53 -0300

    Implementing tests.

--- src/app/list-cep/list-cep.component.ts
+++ src/app/list-cep/list-cep.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Address } from 'node:cluster';
+import { Address } from './address.model';
 import { ListCepService } from './list-cep.service';
 
 @Component({
